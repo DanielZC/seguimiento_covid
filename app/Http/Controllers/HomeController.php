afe26a3bcafd66876623cc753ea5ede25cfeffc7
Victor Daniel Zabaleta Castellar <zabaletacastellar@gmail.com>
2020-07-21 09:46:23 -0500

    se creo el controlador para pacientes

--- app/Http/Controllers/HomeController.php
+++ app/Http/Controllers/HomeController.php
@@ -3,8 +3,14 @@
 namespace App\Http\Controllers;
 
 use Illuminate\Http\Request;
+use App\PacienteModel;
 
 class HomeController extends Controller
 {
-    //
+    public function Index()
+    {
+        $pacientes = new PacienteModel;
+
+        return $pacientes->Listar();
+    }
 }
