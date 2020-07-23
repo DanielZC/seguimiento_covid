a0314b1ad779202c6b6d59594004ccc522f11928
Victor Daniel Zabaleta Castellar <zabaletacastellar@gmail.com>
2020-07-23 14:48:47 -0500

    se crearon las funciones buscar y crear (funcionando)

--- app/ProgramacionTomaDeMuestraModel.php
+++ app/ProgramacionTomaDeMuestraModel.php
@@ -10,23 +10,53 @@ class ProgramacionTomaDeMuestraModel extends Model
     public function Crear($programacion)
     {
         $sql = DB::table('prog_toma_muestra')->insert([
-
+            'pacientes_id' => $programacion->paciente_id,
+            'acepta_visita' => $programacion->acepta_visita,
+            'fecha_programacion' => $programacion->fecha_programacion .' '. date('h:i:s'),
+            'programacion_atencion' =>$programacion->programacion_atencion,
+            'programa_pertenece' =>$programacion->nombre_programa
         ]);
+
+        if($sql)
+        {
+            $result = 'ok';
+        }
+        else
+        {
+            $result = 'bad';
+        }      
+
+        return $result;
     }
 
     public function Buscar($pacienteInfo)
     {
-        $sql = DB::table('pacientes')->where('numero_documento', '=', $pacienteInfo->numero_documento)->get();
+        $result = '';
+        $msj = '';
+
+        $paciente = DB::table('pacientes')->where('numero_documento', '=', $pacienteInfo->numero_documento)->get();
         
-        if ($sql->count() > 0) 
+        foreach ($paciente as $pacienteInfo){}
+
+        if ($paciente->count() != 0) 
         {
-            $result = $sql;
+            $sql = DB::table('prog_toma_muestra')->where('pacientes_id', '=', $pacienteInfo->id)->count();
+
+            if($sql == 0)
+            {
+                $result = $paciente;
+            }
+            else
+            {
+                $result = $pacienteInfo->primer_nombre .' '. $pacienteInfo->primer_apellido .' '. $pacienteInfo->segundo_apellido;
+                $msj = 'exists';
+            }
         }
         else
         {
-            $result = '!found';
+            $msj = '!found';
         }
 
-        return $result;
+        return ['paciente' => $result, 'mensaje' => $msj];
     }
 }
