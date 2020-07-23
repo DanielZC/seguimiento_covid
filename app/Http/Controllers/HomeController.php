5ca0dfdf5da605fccb3680d09cb1a66ed3ba7242
Victor Daniel Zabaleta Castellar <zabaletacastellar@gmail.com>
2020-07-22 23:36:20 -0500

    se modifico HomeController para que se encargue de los proceso de la vista principal

--- app/Http/Controllers/HomeController.php
+++ app/Http/Controllers/HomeController.php
@@ -3,13 +3,11 @@
 namespace App\Http\Controllers;
 
 use Illuminate\Http\Request;
-use App\PacienteModel;
 
 class HomeController extends Controller
 {
     public function Index()
     {
-        $pacientes = new PacienteModel;
-        return $pacientes->Listar();
+        return view('index');
     }
 }
